20e27435107c191bdda0dff9c3c2f7aad7e151c9
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-08-13 15:51:40 +0700

    separating view template and connect to database

--- models/Item.js
+++ models/Item.js
@@ -2,54 +2,60 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
 const itemSchema = new mongoose.Schema({
-    title: {
-      type: String,
-      required: true
-    },
-    price: {
-      type: Number,
-      required: true
-    },
-    country: {
-      type: String,
-      default: 'Indonesia'
-    },
-    city: {
-      type: String,
-      required: true
-    },
-    isPopular: {
-      type: Boolean,
-      default: false
-    },
-    description: {
-      type: String,
-      required: true
-    },
-    unit: {
-      type: String,
-      default: 'night'
-    },
-    sumBooking: {
-      type: Number,
-      default: 0
-    },
-    categoryId: {
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    default: "Indonesia",
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  isPopular: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  unit: {
+    type: String,
+    default: "night",
+  },
+  sumBooking: {
+    type: Number,
+    default: 0,
+  },
+  categoryId: {
+    type: ObjectId,
+    ref: "Category",
+  },
+  imageId: [
+    {
       type: ObjectId,
-      ref: 'Category'
+      ref: "Image",
     },
-    imageId: [{
+  ],
+  featureId: [
+    {
       type: ObjectId,
-      ref: 'Image'
-    }],
-    featureId: [{
+      ref: "Feature",
+    },
+  ],
+  activityId: [
+    {
       type: ObjectId,
-      ref: 'Feature'
-    }],
-    activityId: [{
-      type: ObjectId,
-      ref: 'Activity'
-    }]
-  })
+      ref: "Activity",
+    },
+  ],
+});
 
 module.exports = mongoose.model("Item", itemSchema);
---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+var express = require("express");
+const router = express.Router();
+const adminController = require("../controllers/adminController");
+
+/* GET admin page. */
+router.get("/dashboard", adminController.viewDashboard);
+router.get("/category", adminController.viewCategory);
+router.get("/bank", adminController.viewBank);
+router.get("/item", adminController.viewItem);
+router.get("/booking", adminController.viewBooking);
+
+module.exports = router;
