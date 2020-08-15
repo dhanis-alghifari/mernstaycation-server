8acc0c281d59d40bf77eb561ba45583084ec7bee
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-08-15 22:35:24 +0700

    make crud category

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
+const { route } = require(".");
 
 /* GET admin page. */
 router.get("/dashboard", adminController.viewDashboard);
@@ -8,5 +9,10 @@ router.get("/category", adminController.viewCategory);
 router.get("/bank", adminController.viewBank);
 router.get("/item", adminController.viewItem);
 router.get("/booking", adminController.viewBooking);
+
+/* Endpoint CRUD Category admin page. */
+router.post('/category', adminController.addCategory);
+router.put('/category', adminController.editCategory);
+router.delete('/category/:id', adminController.deleteCategory);
 
 module.exports = router;
