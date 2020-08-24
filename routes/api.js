f164897fcaf9cc93519b293571a149b3dcb59165
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-08-25 00:01:24 +0700

    Create API Booking Page

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,15 @@
 var express = require("express");
 const router = express.Router();
 const apiController = require("../controllers/apiController");
+const { uploadSingle } = require("../middleware/multer");
 
 /* Endpoint API Landing Page */
 router.get("/landing-page", apiController.landingPage);
 
 /* Endpoint API Detail Page */
 router.get("/detail-page/:id", apiController.detailPage);
+
+/* Endpoint API Booking */
+router.post("/booking-page", uploadSingle, apiController.bookingPage);
 
 module.exports = router;
