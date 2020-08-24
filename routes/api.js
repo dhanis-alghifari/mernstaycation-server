0bb3c165d07ff706dc607dd305a4be2e1334ab0e
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-08-24 16:15:57 +0700

    Create API Detail Page

--- routes/api.js
+++ routes/api.js
@@ -5,4 +5,7 @@ const apiController = require("../controllers/apiController");
 /* Endpoint API Landing Page */
 router.get("/landing-page", apiController.landingPage);
 
+/* Endpoint API Detail Page */
+router.get("/detail-page/:id", apiController.detailPage);
+
 module.exports = router;
