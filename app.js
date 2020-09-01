a50f1b4e722056b0ead1bb02ae2ddca524aa6042
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-09-01 20:44:42 +0700

    change duration session

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const methodOverride = require("method-override");
-const session = require('express-session');
-const flash = require('connect-flash');
+const session = require("express-session");
+const flash = require("connect-flash");
 const cors = require("cors");
 // import mongoose
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://mernstaycation:131212@cluster0.efwo8.mongodb.net/db_staycationmern?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://mernstaycation:131212@cluster0.efwo8.mongodb.net/db_staycationmern?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -24,18 +27,19 @@ const adminRouter = require("./routes/admin");
 const apiRouter = require("./routes/api");
 var app = express();
 
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(methodOverride("_method"));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 90000 }
-}));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 900000000 },
+  })
+);
 app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
@@ -55,7 +59,6 @@ app.use(cors());
 app.use("/admin", adminRouter);
 // API
 app.use("/api/v1/member", apiRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
